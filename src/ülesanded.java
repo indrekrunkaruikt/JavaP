7eb826df02da8153a2d8f7f3c91d32a0c9d4ba83
ir2020 <indrek.runkaru@khk.ee>
2020-09-24 21:16:23 +0300

    ülesanne 19

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -3,23 +3,17 @@ import java.util.Scanner;
 public class ülesanded {
 
     public static void main(String[] args) {
-        // Ülesanne 18 Create a program that gives the course grade based on the following table.
+        // Ülesanne 19 Create a program that gives the course grade based on the following table.
         Scanner inputNumber1 = new Scanner(System.in);
-        System.out.print("Type the number of points: ");
+        System.out.print("HOW OLD ARE YOU: ");
         int number = Integer.parseInt(inputNumber1.nextLine());
-        if (number == 29 || number <= 29) {
-            System.out.println("Your FAILED!!!");
-        } else if ( number == 34 || number <=34){
-            System.out.println("Your got 1, bad job.");
-        } else if ( number == 39 || number <= 39){
-            System.out.println("Your got 2, pretty bad job.");
-        } else if ( number == 44 || number <= 44){
-            System.out.println("Your got 3, not great, not terrible.");
-        } else if ( number == 49 || number <= 49){
-            System.out.println("Your got 4, pretty good.");
-        } else if ( number == 60 || number <= 60 ) {
-            System.out.println("You succeeded!");
-        };
+
+        if (number > 0 && number < 125) {
+            System.out.println("OK");
+        } else {
+            System.out.println("Impossible!");
+        }
+
         System.out.println(number);
     }
 }
