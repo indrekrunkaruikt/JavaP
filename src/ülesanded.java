55084035a27adfc4fdd48e8654773b84f72dbc93
ir2020 <indrek.runkaru@khk.ee>
2020-09-24 15:08:52 +0300

    Ülesanne 14

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -3,32 +3,19 @@ import java.util.Scanner;
 public class ülesanded {
 
     public static void main(String[] args) {
-	// write your code here
-        // ülesanne 1
-        System.out.print("Jane Doe \n");
-        // Ülesanne 5
-        Integer days = 365;
-        Integer hours = 24;
-        Integer seconds = 60;
-        Integer allSecondsInAYear = 365 * 24 * 60;
-        System.out.print("There are " + allSecondsInAYear + "Seconds in a year.\n");
-        //Ülesanne 9
-        Scanner numberInput1 = new Scanner(System.in);
-        System.out.print("Type a number A: ");
-        int numbera = Integer.parseInt(numberInput1.nextLine());
-        Scanner numberInput2 = new Scanner(System.in);
-        System.out.print("Type a number B: ");
-        int numberb = Integer.parseInt(numberInput2.nextLine());
-        double  sumOfNumbers = numbera / numberb;
-        System.out.println("The result is: " + sumOfNumbers);
-        // Ülesanne 10;
-        Double PI = 3.14;
-        Scanner inputRadius = new Scanner(System.in);
-        System.out.print("Type a number A: ");
-        int radius = Integer.parseInt(inputRadius.nextLine());
-        double circumference = 2 * PI * radius;
-        System.out.println("The diameter is: " + circumference);
+	// Ülesanne 14
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(inputNumber.nextLine());
 
+        System.out.println("You typed " + number);
+
+
+        if (number > 0) {
+            System.out.println("The number is positive");
+        } else {
+            System.out.println("The number is negative");
+        }
 
     }
 }
