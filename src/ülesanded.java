6e5ce4f7b2da2127db471a9ea45a3e1dddc57724
ir2020 <indrek.runkaru@khk.ee>
2020-09-29 11:34:06 +0300

    Exercise 36.3: Summing and counting the numbers

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -2,21 +2,24 @@ import java.util.Scanner;
 
 public class ülesanded {
     public static void main(String[] args) {
-        // Ülesanne 36
-        int result = 0;
+        // Exercise 36.3: Summing and counting the numbers
+        int sumResult = 0;
+        int sumCount = 0;
         Scanner inputNumber = new Scanner(System.in);
         System.out.print("Type the number of points: ");
         int number = Integer.parseInt(inputNumber.nextLine());
 
         while(true){
             if(number != -1){
-            result = result + number;
+                sumCount +=1;
+            sumResult = sumResult + number;
             number = Integer.parseInt(inputNumber.nextLine());
             }
             else {
                 break;
             }
          }
-        System.out.print(result);
+        System.out.print(sumResult + "\n");
+        System.out.print(sumCount);
     }
 }
