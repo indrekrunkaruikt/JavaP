4e2d589b412fc44b837455efdbf03fe858ba8899
ir2020 <indrek.runkaru@khk.ee>
2020-09-29 09:40:46 +0300

    Ülesanne 29

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -3,22 +3,17 @@ import java.util.Scanner;
 public class ülesanded {
 
     public static void main(String[] args) {
-        // Ülesanne 26
+        // Ülesanne 29
 
-        Scanner reader = new Scanner(System.in);
-        int sum = 0;
-        while (true) {
-            int read = Integer.parseInt(reader.nextLine());
-            if (read == 0) {
-                break;
-            }
-            sum = sum + read;
-            // DO SOMETHING HERE
+        int number = 1;
 
-            System.out.println("Sum now: " + sum);
+        while (number < 100) {
+
+            if (number % 2 == 0)
+                System.out.println(number);
+
+            number++;  // number++ means the same as number = number + 1
+
         }
-
-        System.out.println("Sum in the end: " + sum);
-
     }
 }
