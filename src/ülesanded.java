e6605b09241eafb4a63ae89881a3b94db8ffe461
ir2020 <indrek.runkaru@khk.ee>
2020-09-29 11:29:07 +0300

    Exercise 36.2: The sum of the numbers

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class ülesanded {
     public static void main(String[] args) {
         // Ülesanne 36
-
+        int result = 0;
         Scanner inputNumber = new Scanner(System.in);
         System.out.print("Type the number of points: ");
         int number = Integer.parseInt(inputNumber.nextLine());
 
         while(true){
             if(number != -1){
-                number = Integer.parseInt(inputNumber.nextLine());
-            } else {
+            result = result + number;
+            number = Integer.parseInt(inputNumber.nextLine());
+            }
+            else {
                 break;
             }
-        }
-
+         }
+        System.out.print(result);
     }
 }
