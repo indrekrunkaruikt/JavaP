f97ae820f227f7bbba4df42be601d0a6e77ccc35
ir2020 <indrek.runkaru@khk.ee>
2020-09-29 09:46:50 +0300

    Ülesanne 30

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class ülesanded {
 
     public static void main(String[] args) {
-        // Ülesanne 29
+        // Ülesanne 30
 
         int number = 1;
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.print("Up to how high? : ");
+        int inputNumberMain = Integer.parseInt(inputNumber.nextLine());
 
-        while (number < 100) {
+        while (number <= inputNumberMain) {
 
-            if (number % 2 == 0)
                 System.out.println(number);
-
             number++;  // number++ means the same as number = number + 1
 
         }
