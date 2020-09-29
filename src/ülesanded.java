a98b20176fbc85f912ffb9f67ead895a7d95a09e
ir2020 <indrek.runkaru@khk.ee>
2020-09-29 09:34:10 +0300

    Ülesanne 26

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -3,24 +3,22 @@ import java.util.Scanner;
 public class ülesanded {
 
     public static void main(String[] args) {
-        // Ülesanne 22
+        // Ülesanne 26
 
-        String correctPassword = "carrot";
-        boolean isPasswordCorrect = false;
-        while(!isPasswordCorrect){
-            Scanner passwordInput = new Scanner(System.in);
-            System.out.print("Type in the password: ");
-            String password = passwordInput.nextLine(); // Reads a line of input from the user and assigns it
-            if (!(correctPassword.equals(password))) {  // true if the condition text.equals("milk") is false
-                System.out.println("Wrong!");
-                // System.out.println("Your password is " + password);
-            } else {
-                System.out.println("Right");
-                System.out.println("The secret is: The gold is buried between those 2 stones behind the oak tree.\n");
-                // System.out.println("Your password is " + password);
-                isPasswordCorrect = true;
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine());
+            if (read == 0) {
+                break;
             }
+            sum = sum + read;
+            // DO SOMETHING HERE
+
+            System.out.println("Sum now: " + sum);
         }
+
+        System.out.println("Sum in the end: " + sum);
 
     }
 }
