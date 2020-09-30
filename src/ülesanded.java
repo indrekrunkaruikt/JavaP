de9c5245b3e0e8e995803277671dc00c7cca6ec0
ir2020 <indrek.runkaru@khk.ee>
2020-09-30 13:46:01 +0300

    Ülesanne 39.2

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ülesanded {
-    // Ülesanne 39.1
+    // Ülesanne 39.2
     private static void printStars(int amount) {
         // you can print one star with the command
         String stars = "";
@@ -12,9 +12,13 @@ public class ülesanded {
         System.out.print(stars + "\n");
         // call this command amount times
     }
+    private static void printSquare(int sideSize) {
+        int height = 0;
+        for (int i = 1; i <= sideSize; i++) {
+            printStars(sideSize);
+        }
+    }
     public static void main(String[] args) {
-        printStars(5);
-        printStars(3);
-        printStars(9);
+        printSquare(3);
     }
 }
