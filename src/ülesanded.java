d00a7d5cee0d9a711cc5ae242bc11ad4e38bef87
ir2020 <indrek.runkaru@khk.ee>
2020-09-30 14:13:21 +0300

    guessing a number

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -1,36 +1,26 @@
 import java.util.Scanner;
 
 public class ülesanded {
-    // Exercise 39.4: Printing a left-aligned triangle
-    private static void printStars(int amount) {
-        // you can print one star with the command
-        String stars = "";
-        for (int i =1; i <= amount; i++) {
-            String result = "*";
-            stars = stars + result;
+    // Exercise 41
+    private static void quessingGame(int randomNumber) {
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(inputNumber.nextLine());
+        if(randomNumber == number){
+            System.out.print("Congratulations, your guess is correct!\n");
+        }else if (randomNumber < number){
+            System.out.print("It is smaller\n");
+        }else{
+            System.out.print("It is bigger\n");
         }
-        System.out.print(stars + "\n");
-        // call this command amount times
+
     }
-    private static void printSquare(int sideSize) {
-        for (int i = 1; i <= sideSize; i++) {
-            printStars(sideSize);
-        }
-    }
-    private static void printRectangle(int aSideSize, int bSideSize) {
-        for (int i = 1; i <= bSideSize; i++) {
-            printStars(aSideSize);
-        }
-    }
-    private static void printTriangle(int size) {
-        for (int i = 1; i <= size; i++) {
-            for(int j = 1; j <=i; j++){
-                System.out.print("*");
-            }
-            System.out.print("\n");
-        }
-    }
-        public static void main(String[] args) {
-        printTriangle(4);
+
+    public static void main(String[] args) {
+        int randomNumber = (int)(100.0 * Math.random());
+
+        quessingGame(randomNumber);
+        System.out.print(randomNumber);
+
     }
 }
