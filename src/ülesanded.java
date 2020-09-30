1e792864df294fe72274821c4a54b6de2c2ca6f6
ir2020 <indrek.runkaru@khk.ee>
2020-09-30 13:25:52 +0300

    Ülesanne 39.1

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
 
 public class ülesanded {
-    // Ülesanne 37
+    // Ülesanne 39.1
+    private static void printStars(int amount) {
+        // you can print one star with the command
+        String stars = "";
+        for (int i =1; i <= amount; i++) {
+            String result = "*";
+            stars = stars + result;
+        }
+        System.out.print(stars + "\n");
+        // call this command amount times
+    }
     public static void main(String[] args) {
-        printText();
+        printStars(5);
+        printStars(3);
+        printStars(9);
     }
-
-    public static void printText() {
-        System.out.println("In the beginning there were the swamp, the hoe and Java.");
-    }
-
 }
