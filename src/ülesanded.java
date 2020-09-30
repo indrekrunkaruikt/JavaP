ef50478a3bafa8a00ebffc59ba0907442956ac9e
ir2020 <indrek.runkaru@khk.ee>
2020-09-30 14:16:36 +0300

    Excercise 41.2 Repeated quessing

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class ülesanded {
     // Exercise 41
-    private static void quessingGame(int randomNumber) {
-        Scanner inputNumber = new Scanner(System.in);
-        System.out.print("Type a number: ");
-        int number = Integer.parseInt(inputNumber.nextLine());
-        if(randomNumber == number){
-            System.out.print("Congratulations, your guess is correct!\n");
-        }else if (randomNumber < number){
-            System.out.print("It is smaller\n");
-        }else{
-            System.out.print("It is bigger\n");
-        }
 
-    }
 
     public static void main(String[] args) {
         int randomNumber = (int)(100.0 * Math.random());
+        while(true){
+                Scanner inputNumber = new Scanner(System.in);
+                System.out.print("Type a number: ");
+                int number = Integer.parseInt(inputNumber.nextLine());
+                if(randomNumber == number){
+                    System.out.print("Congratulations, your guess is correct!\n");
+                    break;
+                }else if (randomNumber < number){
+                    System.out.print("It is smaller\n");
+                }else{
+                    System.out.print("It is bigger\n");
+                }
 
-        quessingGame(randomNumber);
-        System.out.print(randomNumber);
+
+        }
 
     }
 }
