8298b9a79e4433b899b845415d60e59080448157
ir2020 <indrek.runkaru@khk.ee>
2020-09-30 11:37:14 +0300

    ülesanne 37

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -1,40 +1,13 @@
 import java.util.Scanner;
 
 public class ülesanded {
+    // Ülesanne 37
     public static void main(String[] args) {
-        // Exercise 36.5: Counting the average
-        double sumResult = 0;
-        double sumCount = 0;
-        double average = 0;
-        int even = 0;
-        int odd = 0;
-        Scanner inputNumber = new Scanner(System.in);
-        System.out.print("Type the number of points: ");
-        double number = Integer.parseInt(inputNumber.nextLine());
-
-        while(true){
-            if(number != -1){
-                sumCount +=1;
-            sumResult = sumResult + number;
-            if(number % 2 == 0){
-                even +=1;
-            } else {
-                odd +=1;
-            }
-
-            number = Integer.parseInt(inputNumber.nextLine());
-
-            }
-            else {
-                break;
-            }
-         }
-        average = sumResult / sumCount;
-        System.out.print(sumResult + "\n");
-        System.out.print(sumCount  + "\n");
-        System.out.print("Average numbers" + average  + "\n");
-        System.out.print("Even numbers " + even  + "\n");
-        System.out.print("Odd numbers " + odd  + "\n");
-
+        printText();
     }
+
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+
 }
