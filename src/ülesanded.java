322a98e87e6a3cc85c9af3581d8fe644cbb0ff6d
ir2020 <indrek.runkaru@khk.ee>
2020-10-01 12:51:53 +0300

    Excercise 50

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
 public class ülesanded {
-    // Exercise 44
-
-    public static double average(double number1, double number2, double number3, double number4) {
-        return (number1 + number2 + number3 + number4) / 4;
-    }
-
+    // Exercise 50: Separating first characters
     public static void main(String[] args) {
-        double answer = average(4, 3, 6, 1);
-        System.out.println("average: " + answer);
-    }
-}
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.print("Type a name : ");
+        String name = inputNumber.nextLine();
+        int nameLength = name.length();
+
+        for(int i=1;i<=nameLength;i++){
+            char character = name.charAt(i);
+            System.out.println(i + "character: " + character);
+
+        }
+
+    }}
+
+
