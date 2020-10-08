81c3855d0ae9d0a41bfe1ef4f9d76b2ed02455ba
ir2020 <indrek.runkaru@khk.ee>
2020-10-08 15:28:43 +0300

    Exercise 56: Reversing text

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
 
+
 public class ülesanded {
-    // Exercise 53: First part
+
+    // Exercise 56: Reversing text
+    public static String reverse(String text) {
+        String reverseText = "";
+        int i = text.length()-1;
+        while(i >= 0){
+            char symbol = text.charAt(i);
+            reverseText += symbol;
+            i--;
+        }
+        return reverseText;
+    }
+
     public static void main(String[] args) {
 
-        Scanner inputWord = new Scanner(System.in);
-        System.out.print("Type a word : ");
-        String word = inputWord.nextLine();
-
-        Scanner inputLength = new Scanner(System.in);
-        System.out.print("Type the number of words that come from the beginning : ");
-        int number = inputLength.nextInt();
-
-        System.out.println(word.substring(0, number));
-
+        System.out.print("Type in your text: ");
+        Scanner reader = new Scanner(System.in);
+        String text = reader.nextLine();
+        System.out.println("In reverse order: " + reverse(text));
     }}
 
 
