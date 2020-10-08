e3b77c9904121fe1c69378cae91387d397672d10
ir2020 <indrek.runkaru@khk.ee>
2020-10-08 19:40:06 +0300

    Excercise 69, Palindrome

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -4,21 +4,37 @@ import java.util.ArrayList;
 
 public class ülesanded {
 
-    // Exercise 66: The Greatest
-    public static int greatest(ArrayList<Integer> list) {
-       return Collections.max(list);
+    // Exercise 69: Palindrome
+    public static boolean palindrome(String text) {
+        String reverseText = "";
+        int i = text.length()-1;
+        while(i >= 0){
+            char symbol = text.charAt(i);
+            reverseText += symbol;
+            i--;
+        }
+        System.out.println(reverseText + " " + text);
+        if(reverseText != text){
+            return true;
+        } else{
+            return false;
+        }
+
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(3);
-        list.add(2);
-        list.add(7);
-        list.add(2);
-        System.out.println("The greatest number is: " + greatest(list));
-    }
-    }
+        Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type a text: ");
+        String text = reader.nextLine();
+        if (palindrome(text)) {
+            System.out.println("The text is a palindrome!");
+        } else {
+            System.out.println("The text is not a palindrome!");
+        }
+
+    }
+}
 
 
 
