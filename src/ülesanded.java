0076bd9a418efa3c109e27609647c7e8d57f7091
ir2020 <indrek.runkaru@khk.ee>
2020-10-08 15:36:42 +0300

    Excercise 57: Words

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class ülesanded {
 
-    // Exercise 56: Reversing text
-    public static String reverse(String text) {
-        String reverseText = "";
-        int i = text.length()-1;
-        while(i >= 0){
-            char symbol = text.charAt(i);
-            reverseText += symbol;
-            i--;
-        }
-        return reverseText;
-    }
+    // Exercise 57: Words
 
     public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<String>();
+        while(true){
+            System.out.print("Type a word: ");
+            Scanner reader = new Scanner(System.in);
+            String text = reader.nextLine();
+            words.add(text);
+            if ( text.isEmpty() ) {  // could also be: word.equals("")
+                for (String word : words) {
+                    System.out.println( word );
+                }
+                break;
+            }
 
-        System.out.print("Type in your text: ");
-        Scanner reader = new Scanner(System.in);
-        String text = reader.nextLine();
-        System.out.println("In reverse order: " + reverse(text));
+        }
     }}
 
 
