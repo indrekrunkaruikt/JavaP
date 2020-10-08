f83a2f3e2139f1e458900f0107792174d45a0212
ir2020 <indrek.runkaru@khk.ee>
2020-10-08 17:36:52 +0300

    Excercise 58: Recurring words

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -3,23 +3,28 @@ import java.util.ArrayList;
 
 public class ülesanded {
 
-    // Exercise 57: Words
+    // Exercise 58: Recurring word
 
     public static void main(String[] args) {
         ArrayList<String> words = new ArrayList<String>();
+
         while(true){
             System.out.print("Type a word: ");
             Scanner reader = new Scanner(System.in);
             String text = reader.nextLine();
-            words.add(text);
-            if ( text.isEmpty() ) {  // could also be: word.equals("")
+
+            if ( words.contains(text) ) {  // could also be: word.equals("")
                 for (String word : words) {
                     System.out.println( word );
                 }
                 break;
             }
+            words.add(text);
 
         }
     }}
+
+
+
 
 
