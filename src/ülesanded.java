c645879dd3f24512a98e8ada4761ba710e3431b0
ir2020 <indrek.runkaru@khk.ee>
2020-10-08 15:00:25 +0300

    Exercise 53: First part

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class ülesanded {
-    // Exercise 50: Separating first characters
+    // Exercise 53: First part
     public static void main(String[] args) {
-        Scanner inputNumber = new Scanner(System.in);
-        System.out.print("Type a name : ");
-        String name = inputNumber.nextLine();
-        int nameLength = name.length();
 
-        for(int i=1;i<=nameLength;i++){
-            char character = name.charAt(i);
-            System.out.println(i + "character: " + character);
+        Scanner inputWord = new Scanner(System.in);
+        System.out.print("Type a word : ");
+        String word = inputWord.nextLine();
 
-        }
+        Scanner inputLength = new Scanner(System.in);
+        System.out.print("Type the number of words that come from the beginning : ");
+        int number = inputLength.nextInt();
+
+        System.out.println(word.substring(0, number));
 
     }}
 
