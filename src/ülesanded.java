999ebe97ec74069915e77f60d492200230989784
ir2020 <indrek.runkaru@khk.ee>
2020-10-08 17:42:11 +0300

    Exercise 60: Words in alphabetical order

--- src/ülesanded.java
+++ src/ülesanded.java
@@ -1,9 +1,10 @@
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ülesanded {
 
-    // Exercise 58: Recurring word
+    // Exercise 60: Words in alphabetical order
 
     public static void main(String[] args) {
         ArrayList<String> words = new ArrayList<String>();
@@ -12,7 +13,7 @@ public class ülesanded {
             System.out.print("Type a word: ");
             Scanner reader = new Scanner(System.in);
             String text = reader.nextLine();
-
+            Collections.sort(words);
             if ( words.contains(text) ) {  // could also be: word.equals("")
                 for (String word : words) {
                     System.out.println( word );
