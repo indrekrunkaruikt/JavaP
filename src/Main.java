63ddb6414e0375bb10cd8e43ed32d2d5d5d36671
ir2020 <indrek.runkaru@khk.ee>
2020-10-10 09:19:24 +0300

    Excercise 79.3: Asking for numbers from the user

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        NumberStatistics stats = new NumberStatistics();
+        System.out.println("Type a number: ");
+        while (true){
+            int inputNumber =  Integer.parseInt(reader.nextLine());
+            stats.addNumber(inputNumber);
+            if(inputNumber == -1){
+                break;
+            }
+        }
+        System.out.println("Amount: " + stats.amountOfNumbers());
+        System.out.println("sum: " + stats.sum());
+        System.out.println("average: " + stats.average());
+        /*
+
         NumberStatistics stats = new NumberStatistics();
         stats.addNumber(3);
         stats.addNumber(5);
         stats.addNumber(1);
         stats.addNumber(2);
-        System.out.println("Amount: " + stats.amountOfNumbers());
+
+        */
     }
 }
--- src/NumberStatistics.java
+++ src/NumberStatistics.java
@@ -1,5 +1,6 @@
 public class NumberStatistics {
     private int amountOfNumbers;
+    private int sumOfNumbers = 0;
 
     public NumberStatistics() {
         // initialize here the object variable amountOfNumber
@@ -7,11 +8,22 @@ public class NumberStatistics {
 
     public void addNumber(int number) {
         // code here
+        this.sumOfNumbers = this.sumOfNumbers + number;
         this.amountOfNumbers = this.amountOfNumbers + 1;
     }
 
     public int amountOfNumbers() {
         // code here
         return this.amountOfNumbers;
+    }
+    public int sum() {
+        // code here
+        return this.sumOfNumbers;
+    }
+
+    public double average() {
+
+        // code here
+        return (double)this.sum()/ this.amountOfNumbers();
     }
 }
