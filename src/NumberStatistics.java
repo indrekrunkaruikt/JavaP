f14ade2e1f339db697868bb484e398feca5f900c
ir2020 <indrek.runkaru@khk.ee>
2020-10-10 08:56:46 +0300

    Excercise 79.2 sum and average

---
+++ src/NumberStatistics.java
@@ -0,0 +1,17 @@
+public class NumberStatistics {
+    private int amountOfNumbers;
+
+    public NumberStatistics() {
+        // initialize here the object variable amountOfNumber
+    }
+
+    public void addNumber(int number) {
+        // code here
+        this.amountOfNumbers = this.amountOfNumbers + 1;
+    }
+
+    public int amountOfNumbers() {
+        // code here
+        return this.amountOfNumbers;
+    }
+}
