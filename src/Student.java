e265ad2e52df5bef142500011ece1f643357fd76
ir2020 <indrek.runkaru@khk.ee>
2020-10-12 09:16:23 +0300

    Excercise 88.1: Class student

---
+++ src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private String name;
+    private String studentNumber ;
+
+    public Student(String name, String studentNumber){
+        this.name = name;
+        this.studentNumber = studentNumber;
+    }
+    public String getName(){
+
+        return this.name;
+    }
+    public String getStudentNumber(){
+        return this.studentNumber;
+    }
+    public String toString(){
+        String returnMessage = getName() + getStudentNumber();
+        return returnMessage;
+    }
+}
