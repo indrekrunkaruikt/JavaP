0ce62bc03ddeca3cf3d17cf8ac4070b79d23a7d2
ir2020 <indrek.runkaru@khk.ee>
2020-10-12 18:31:20 +0300

    Excercise 89: Clock object

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Student> list = new ArrayList<Student>();
-        Scanner reader = new Scanner(System.in);
-        String studentName = "";
-        String studentNumber = "";
-        String searchInputValue = "";
-        while(true){
-            System.out.println("Name: ");
-            studentName = reader.nextLine();
-            System.out.println("studentNumber: ");
-            studentNumber = reader.nextLine();
-            if(studentName == "" ){
-                for (Student StudentItem : list) {
-                    System.out.println(StudentItem);
-                }
-                break;
-            } else{
-                list.add( (new Student(studentName, studentNumber)));
-            }
-        }
-        System.out.println("Search: ");
-        searchInputValue = reader.nextLine();
-        while(true){
-            for (Student StudentItem : list) {
-                if(StudentItem.getName().contains(searchInputValue)){
-                    System.out.println(StudentItem);
-                }
-            }
-            break;
-        }
+        Clock clock = new Clock(23, 59, 50);
 
-
+        int i = 0;
+        while( i < 20) {
+            System.out.println( clock );
+            clock.tick();
+            i++;
+        }
     }
 }
