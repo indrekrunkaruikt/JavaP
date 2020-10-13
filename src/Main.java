246ef6ee378d625d9ba9d0b5d5d128da18253f7a
ir2020 <indrek.runkaru@khk.ee>
2020-10-13 18:54:21 +0300

    Java alused kontrolltöö -Ülesanne 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-import java.util.Scanner;
-
 public class Main{
-    int nullvalue = 0;
     public static void main(String[] args) {
-        int i = 10;
-
-        for (int j = 0; j < i; j++) {
-            for (int k = 0; k < i; k++) {
-                System.out.print(j);
+        int i = -1;
+        for (int j = 9; j > i; j--) {
+            for (int k = j; k > i; k--) {
+                System.out.print(k + " ");
             }
             System.out.print("\n");
         }
